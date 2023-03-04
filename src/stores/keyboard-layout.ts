d0a687adac0f74b3e8986b5fb95427902bc9cac6
edikabe <jglotain@gmail.com>
2023-03-04 15:34:59 +0100

    fixed linter failed

--- src/stores/keyboard-layout.ts
+++ src/stores/keyboard-layout.ts
@@ -1,8 +1,8 @@
 import { defineStore } from 'pinia'
 
 export const useKeyBoardLayoutStore = defineStore('keyboard-layout', () => {
-  const minOctave = -1
-  const maxOctave = 8
+  // const minOctave = -1
+  // const maxOctave = 8
 
   const numberOfOctavesDisplayed = useLocalStorage('mls-settings-octaves-displayed', 2)
 
